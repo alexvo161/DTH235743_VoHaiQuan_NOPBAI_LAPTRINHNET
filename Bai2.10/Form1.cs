94e7ab6e7e8749de00db62ee2d004063371a92ea
Admin <Admin@MT0418>
2025-09-12 16:08:28 +0700

    Add project files.

---
+++ Bai2.10/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai2._10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b;
+
+            if (int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b))
+            {
+                int tong = (a + b) * 2;
+                MessageBox.Show(tong.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập số nguyên hợp lệ!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a, b;
+
+            if (int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b))
+            {
+                int tong = (a * b);
+                MessageBox.Show(tong.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập số nguyên hợp lệ!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a, b;
+
+            if (int.TryParse(textBox1.Text, out a) && int.TryParse(textBox2.Text, out b))
+            {
+                double tong = Math.Sqrt(a * a + b * b);
+                MessageBox.Show(tong.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập số nguyên hợp lệ!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
